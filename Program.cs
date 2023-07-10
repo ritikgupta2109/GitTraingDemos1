9a9d0a579b4e141757f46dc809e9f1fbdd6989ff
ritikgupta2109 <ritikgupta2109@gmail.com>
2023-07-10 12:54:39 +0530

    adding files

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test obj = new Test();
+
+            Console.WriteLine("Enter the number 1");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the number 2");
+
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            obj.divide(num1, num2);
+        }
+
+    }
+}
+    
+
---
+++ Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    class Test
+    {
+        int a, b, c;
+
+        public void divide(int a, int b)
+        {
+            try
+            {
+                int result = a / b;
+                Console.WriteLine($"result is {result}");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
